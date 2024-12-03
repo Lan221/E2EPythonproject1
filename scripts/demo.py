5c9253a0dbfe9e28148b316aa2e742158531f2aa
lan <tina.lan.chen@gmail.com>
2024-12-03 16:47:13 +0100

    initial version Pythonproject 1 want to know how to initial the version

--- scripts/demo.py
+++ scripts/demo.py
@@ -2,7 +2,7 @@
 # type属性设置为类属性值为狗; 设置一个实例方法eat()，打印狗吃骨头(放入一个模块中-模块名tools)
 # b.重新创建一个demo.py文件，引入tools模块，然后定义一个Dog类，继承Animal，重写里面的eat()，
 # 在原有基础上（狗吃骨头）, 添加打印内容;吃完骨头摇摇头...
-
+//version 1
 from tools import Animal
 class Dog(Animal):
     def __init__(self,name,age=0):
--- scripts/test_class.py
+++ scripts/test_class.py
@@ -1,5 +1,5 @@
 class Person():
-
+//version 1
     def __init__(self,name,age):
         self.name = name
         self.age = age
